8ffb00e4b518e6980da71cd118a4276a5d9b8242
Sergey Zalubovskiy <osmosegw@gmail.com>
2017-02-16 14:05:37 +0300

    fixed issue with html5 mode reroute caused by proxy

--- src/app/app.routes.config.js
+++ src/app/app.routes.config.js
@@ -5,7 +5,7 @@ export default routesConfig;
 
 /** @ngInject */
 function routesConfig($stateProvider, $urlRouterProvider, $locationProvider) {
-  // $locationProvider.html5Mode(true).hashPrefix('!');
+  $locationProvider.html5Mode(true).hashPrefix('!');
   $urlRouterProvider.otherwise('/');
 
   $stateProvider
